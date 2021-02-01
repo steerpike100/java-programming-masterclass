184acdbe02b1d3473f897ed551fd167bdc49408a
steveb <stevejbartholomew@gmail.com>
2021-02-01 09:16:34 +0000

    67

---
+++ src/SumThreeAndFive.java
@@ -0,0 +1,22 @@
+public class SumThreeAndFive {
+
+    public static void main(String[] args) {
+
+        int count = 0;
+        int sum = 0;
+
+        for (int i = 1; i <= 1000; i++) {
+            if ((i % 3 == 0) && (i % 5 == 0)) {
+                count++;
+                sum += i;
+                System.out.println(i);
+
+            }
+
+            if (count >= 5) {
+                System.out.println(sum);
+                break;
+            }
+        }
+    }
+}
